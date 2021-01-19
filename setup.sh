ef2526d1735faedd25e9928cd03558acc4c55fc7
Gilda Marva <gmarva@et-f4.kzn.21-school.ru>
2021-01-19 14:56:27 +0300

    nginxphpwp

---
+++ setup.sh
@@ -0,0 +1,3 @@
+minikube start --vm-driver=hyperkit
+minikube dashboard
+minikube addons enable metallb
--- srcs/wordpress/srcs_wordpress/wp-config.php
+++ srcs/wordpress/srcs_wordpress/wp-config.php
@@ -24,13 +24,13 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'gmarva' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'gmarva' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql-service:3306');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
